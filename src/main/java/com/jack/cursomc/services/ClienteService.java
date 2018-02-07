9f62138e5b40e053fb0b94f128360d19328a7f8e
Jailton Ventura <jottha_2006@hotmail.com>
2018-02-06 21:48:48 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/jack/cursomc/services/ClienteService.java
@@ -0,0 +1,31 @@
+package com.jack.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jack.cursomc.domain.Cliente;
+import com.jack.cursomc.repositories.ClienteRepository;
+import com.jack.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class ClienteService
+{	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	public Cliente buscar(Integer id)
+	{	
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null)
+		{
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id 
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+}
